dcfc9efb85b9cf25ec6939d56facf4d0813b2851
Steve Arditti <steven.arditti@gmail.com>
2020-12-08 17:02:49 -0500

    some changes idk

--- run_test.py
+++ run_test.py
@@ -3,32 +3,18 @@ import random
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 
+import matplotlib.pyplot as plt
+
 ARTISTS = ['Kendrick Lamar', 'The Beatles', 'Led Zeppelin', "Aerosmith", "Frank Sinatra", "Kanye West", "Eminem", "Red Hot Chili Peppers", "Queen", "Billy Joel", "Madonna", "Drake"]
 
 
-def print_metrics(model, songs):
-    print(f"\nAccuracy for {model}:")
-    correct = incorrect = 0
-    song_count = {artist: 0 for artist in ARTISTS}
-    predicted = []
-    true_labels = []
-    for song in songs:
-        predicted_artist = model.classify(artist_classifier.preprocess_lyrics(song.lyrics))
-        predicted.append(predicted_artist)
-        true_labels.append(song.artist)
+def plots_data(data):
 
-        if song.artist == predicted_artist:
-            correct += 1
-        else:
-            incorrect += 1
-        song_count[predicted_artist] += 1
+    for data_set in data:
+        plt.boxplot(data_set)
 
-    accuracy = artist_classifier.accuracy(predicted, true_labels)
-    print(f"Accuracy: {accuracy}", )
-    print(f"F score: {artist_classifier.f1_score(predicted, true_labels)}")
-    for artist in ARTISTS:
-        print(artist + ": " + str(song_count[artist]))
-    print(f"{correct} / {len(songs)}")
+    plt.show()
+    plt.savefig('box_accuracy.png')
 
 def metrics(model, songs):
     predicted = []
@@ -39,10 +25,14 @@ def metrics(model, songs):
         true_labels.append(song.artist)
     print(f"{model}:")
     print(classification_report(true_labels, predicted, digits=3, zero_division=0))
+    return classification_report(true_labels, predicted, digits=3, zero_division=0, output_dict=True)
+
 
 def main():
     songs = artist_classifier.load_data()
     random.shuffle(songs)
+
+    data = [[], [], []]
 
     # Used to clean the input songs from genius
     cleaned_lyrics = [" ".join(artist_classifier.lines_from_song(song.lyrics)) for song in songs]
@@ -51,10 +41,12 @@ def main():
 
     train_data, test_data = train_test_split(songs)
 
+
     print("Training bag of words classifier...")
     bow_classifier = artist_classifier.Bag_of_Words_Artist_Classifier("Test Bag-of-Words", ARTISTS)
     bow_classifier.train(train_data)
     print("BOW done.")
+    
 
     print("Training logistic regression classifier...")
     lr_classifier = artist_classifier.Logistic_Regression_Artist_Classifier("Test LogRes", ARTISTS)
@@ -64,12 +56,18 @@ def main():
     print("Training neural network classifier...")
     ffnn_classifier = artist_classifier.Feed_Forward_Neural_Net_Artist_Classifier("Test FFNN", ARTISTS)
     ffnn_classifier.train(train_data)
+    print("NN done.")
 
-    metrics(bow_classifier, test_data)
+
+    bow_report = metrics(bow_classifier, test_data)
+    data[0].append(bow_report['accuracy'])
     
-    metrics(lr_classifier, test_data)
+    lr_report = metrics(lr_classifier, test_data)
+    data[1].append(lr_report['accuracy'])
 
-    metrics(ffnn_classifier, test_data)
+    nn_report = metrics(ffnn_classifier, test_data)
+    data[2].append(nn_report['accuracy'])
+
 
 
 if __name__ == '__main__':
